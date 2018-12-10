fe8faf07ff9b34ed14e026ae66622d06e478f600
王宏 <wanghong3@dafycredit.com>
2018-12-10 17:50:14 +0800

    ByteArrayUtils

---
+++ hong-application-common/src/main/java/com/hong/common/utils/ByteArrayUtils.java
@@ -0,0 +1,39 @@
+package com.hong.common.utils;
+
+import java.io.*;
+import java.util.Optional;
+
+/**
+ * <br>java对象和byte数组互转</br>
+ *
+ */
+public class ByteArrayUtils {
+
+    public static<T> Optional<byte[]> objectToBytes(T obj){
+        byte[] bytes = null;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectOutputStream sOut;
+        try {
+            sOut = new ObjectOutputStream(out);
+            sOut.writeObject(obj);
+            sOut.flush();
+            bytes= out.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Optional.ofNullable(bytes);
+    }
+
+    public static<T> Optional<T> bytesToObject(byte[] bytes) {
+        T t = null;
+        ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+        ObjectInputStream sIn;
+        try {
+            sIn = new ObjectInputStream(in);
+            t = (T)sIn.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Optional.ofNullable(t);
+    }
+}
