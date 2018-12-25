7e5d367e3506dc945de5e61a2d9124e047a45d0e
王宏 <wanghong3@dafycredit.com>
2018-12-25 15:27:59 +0800

    Java8的多重注解

--- hong-application-common/src/main/java/com/hong/common/validate/ValidateFiled.java
+++ hong-application-common/src/main/java/com/hong/common/validate/ValidateFiled.java
@@ -1,12 +1,10 @@
 package com.hong.common.validate;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+@Repeatable(ValidateGroup.class)
 public @interface ValidateFiled {
 	
 	/**
--- hong-application-common/src/main/java/com/hong/common/validate/ValidateGroup.java
+++ hong-application-common/src/main/java/com/hong/common/validate/ValidateGroup.java
@@ -5,8 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * java的多重注解（重复注解） Java8之后新增的特性
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface ValidateGroup {
-    ValidateFiled[] fileds();
+    ValidateFiled[] value();
 }  
